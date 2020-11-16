4beb6fd0846fab7ad4c6187e6c26b31187cbc8d5
lifars-jarka <74553884+lifars-jarka@users.noreply.github.com>
2020-11-16 18:26:48 +0100

    config

--- logchecker/__main__.py
+++ logchecker/__main__.py
@@ -1,5 +1,31 @@
+import argparse
+import configparser
+import collections
+
+Config = collections.namedtuple("Config", ["url", "key"])
+
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", help="Config file path", type=argparse.FileType("r"), required=True
+    )
+    parser.add_argument(
+        "-f", help="Log file path", type=argparse.FileType("r"), required=True
+    )
+    args = parser.parse_args()
+
+    config = parse_config_file(args.c)
+
+def parse_config_file(file):
+    config = configparser.ConfigParser()
+    config.read_file(file)
+    url = config.get("DEFAULT", "url")
+    key = config.get("DEFAULT", "api_key")
+    return Config(url, key)
+
+
 
 
 if __name__ == "__main__":
