819a315101bca2d9f095772d613ffb3a32e6606a
lifars-jarka <74553884+lifars-jarka@users.noreply.github.com>
2020-11-20 17:20:41 +0100

    add output file

--- logchecker/__main__.py
+++ logchecker/__main__.py
@@ -6,7 +6,6 @@ from logchecker.log_checker import check_log_file
 Config = collections.namedtuple("Config", ["url", "key"])
 
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -15,10 +14,12 @@ def main():
     parser.add_argument(
         "-f", help="Log file path", type=argparse.FileType("r"), required=True
     )
+    parser.add_argument("-o", help="Output file path", type=argparse.FileType("w+"))
     args = parser.parse_args()
 
     config = parse_config_file(args.c)
-    check_log_file(args.f, config.url, config.key)
+    check_log_file(args.f, config.url, config.key, args.o)
+
 
 def parse_config_file(file):
     config = configparser.ConfigParser()
@@ -26,8 +27,6 @@ def parse_config_file(file):
     url = config.get("DEFAULT", "url")
     key = config.get("DEFAULT", "api_key")
     return Config(url, key)
-
-
 
 
 if __name__ == "__main__":
--- logchecker/log_checker.py
+++ logchecker/log_checker.py
@@ -3,14 +3,20 @@ import pyeti
 import re
 
 
-def check_log_file(file, url, key):
+def check_log_file(file, url, key, output=None):
     addresses = parse_log_file(file)
     api = pyeti.YetiApi(url, api_key=key)
     for adr, logs in addresses.items():
         result = {"address": adr}
         result["yeti"] = api.observable_search(value=adr)
         result["logs"] = logs
-        print(json.dumps(result, indent=4, sort_keys=True))
+        if output:
+            json.dump(result, output, indent=4, sort_keys=True)
+        else:
+            print(json.dumps(result, indent=4, sort_keys=True))
+            
+    if output:
+        output.close()
 
 
 def parse_log_file(file):
